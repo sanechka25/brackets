17ca3e18fbd7f39a627e2cd4716e4b62ef33d4ec
Alexandra <65849986+sanechka25@users.noreply.github.com>
2021-03-12 12:56:18 +0300

    docs: upload file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var chars = str.split(''),
+        stack = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        closeIndex,
+        openIndex;
+
+    for (var i = 0, len = chars.length; i < len; i++) {
+       openIndex = open.indexOf(chars[i]);
+       if (openIndex !== -1) {
+           stack.push(openIndex);
+           continue;
+       }
+
+       closeIndex = close.indexOf(chars[i]);
+       if (closeIndex !== -1) {
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+       }
+    }
+
+    if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
+
